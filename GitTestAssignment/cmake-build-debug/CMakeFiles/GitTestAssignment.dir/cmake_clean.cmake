6c294a178dc8f5b871b3833986d228b097cb7af8
kevjense <KJ384307>
2020-04-23 15:14:53 -0600

    Git Test Assignment

---
+++ GitTestAssignment/cmake-build-debug/CMakeFiles/GitTestAssignment.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitTestAssignment.dir/main.cpp.o"
+  "GitTestAssignment.exe"
+  "GitTestAssignment.pdb"
+  "libGitTestAssignment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GitTestAssignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
